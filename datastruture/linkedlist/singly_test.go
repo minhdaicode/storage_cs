93f54b3bf413f299ab07f54a8cd0c91a34edd17c
Bui Dai <daispectre@gmail.com>
2024-11-24 22:59:37 +0700

    add test case for Singly Linked List

--- datastruture/linkedlist/singly_test.go
+++ datastruture/linkedlist/singly_test.go
@@ -1,6 +1,8 @@
 package linkedlist_test
 
 import (
+	"fmt"
+	"reflect"
 	"storage_cs/datastruture/linkedlist"
 	"testing"
 )
@@ -10,7 +12,20 @@ func TestSingly_InsertHead(t *testing.T) {
 	l.InsertHead(3)
 	l.InsertHead(2)
 	l.InsertHead(1)
-	l.Tranverse()
+
+	want := []int{1, 2, 3}
+	got := []int{}
+	cur := l.Head
+	got = append(got, cur.Data)
+
+	for cur.Next != nil {
+		cur = cur.Next
+		got = append(got, cur.Data)
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got: %v , want: %v", got, want)
+	}
 }
 
 func TestSingly_InsertTail(t *testing.T) {
@@ -18,15 +33,178 @@ func TestSingly_InsertTail(t *testing.T) {
 	l.InsertTail(3)
 	l.InsertTail(2)
 	l.InsertTail(1)
-	l.Tranverse()
+
+	want := []int{3, 2, 1}
+	got := []int{}
+	cur := l.Head
+	got = append(got, cur.Data)
+
+	for cur.Next != nil {
+		cur = cur.Next
+		got = append(got, cur.Data)
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got: %v , want: %v", got, want)
+	}
 }
 
 func TestSingly_Insert(t *testing.T) {
 	l := linkedlist.NewSingly[int]()
+
+	err := l.Insert(12, -2)
+	if err != linkedlist.ErrPosOutOfRange {
+		t.Errorf("something went wrong")
+	}
+
 	l.Insert(3, 0)
 	l.Insert(2, 1)
 	l.Insert(1, 2)
 	l.Insert(14, 3)
 	l.Insert(25, 1)
+
+	want := []int{3, 25, 2, 1, 14}
+	got := []int{}
+	cur := l.Head
+	got = append(got, cur.Data)
+
+	for cur.Next != nil {
+		cur = cur.Next
+		got = append(got, cur.Data)
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got: %v , want: %v", got, want)
+	}
+
+	err = l.Insert(12, 7)
+	if err != linkedlist.ErrPosOutOfRange {
+		t.Errorf("wsomething went wrong")
+	}
+
 	l.Tranverse()
+	fmt.Println()
+	fmt.Printf("size of list: %v", l.Len())
+}
+
+func TestSingly_RemoveHead(t *testing.T) {
+	l := linkedlist.NewSingly[int]()
+
+	err := l.RemoveHead()
+	if err != linkedlist.ErrListEmpty {
+		t.Errorf("something went wrong")
+	}
+
+	l.InsertHead(3)
+	l.InsertHead(2)
+	l.InsertHead(1)
+
+	err = l.RemoveHead()
+	if err != nil {
+		t.Errorf("something went wrong")
+	}
+
+	want := []int{2, 3}
+	got := []int{}
+	cur := l.Head
+	got = append(got, cur.Data)
+
+	for cur.Next != nil {
+		cur = cur.Next
+		got = append(got, cur.Data)
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got: %v , want: %v", got, want)
+	}
+}
+
+func TestSingly_RemoveTail(t *testing.T) {
+	l := linkedlist.NewSingly[int]()
+
+	err := l.RemoveTail()
+	if err != linkedlist.ErrListEmpty {
+		t.Errorf("something went wrong")
+	}
+
+	l.InsertHead(3)
+
+	err = l.RemoveTail()
+	if err != nil {
+		t.Errorf("something went wrong")
+	}
+
+	l.InsertHead(2)
+	l.InsertHead(1)
+	l.InsertTail(12)
+	l.InsertTail(5)
+
+	err = l.RemoveTail()
+	if err != nil {
+		t.Errorf("something went wrong")
+	}
+
+	want := []int{1, 2, 12}
+	got := []int{}
+	cur := l.Head
+	got = append(got, cur.Data)
+
+	for cur.Next != nil {
+		cur = cur.Next
+		got = append(got, cur.Data)
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got: %v , want: %v", got, want)
+	}
+}
+
+func TestSingly_Remove(t *testing.T) {
+	l := linkedlist.NewSingly[int]()
+
+	err := l.Remove(-2)
+	if err != linkedlist.ErrPosOutOfRange {
+		t.Errorf("something went wrong")
+	}
+
+	l.InsertHead(3)
+
+	err = l.Remove(0)
+	if err != nil {
+		t.Errorf("something went wrong")
+	}
+
+	l.InsertHead(2)
+	l.InsertHead(1)
+	l.InsertTail(12)
+	l.InsertTail(5)
+
+	err = l.Remove(4)
+	if err != nil {
+		t.Errorf("something went wrong")
+	}
+
+	err = l.Remove(7)
+	if err != linkedlist.ErrPosOutOfRange {
+		t.Errorf("something went wrong")
+	}
+
+	err = l.Remove(1)
+	if err != nil {
+		t.Errorf("something went wrong")
+	}
+
+	want := []int{1, 12}
+	got := []int{}
+	cur := l.Head
+	got = append(got, cur.Data)
+
+	for cur.Next != nil {
+		cur = cur.Next
+		got = append(got, cur.Data)
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got: %v , want: %v", got, want)
+	}
 }
